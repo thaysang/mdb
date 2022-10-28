04fa9abd948ed2da5d5b327c5a52d66ce2cd24f9
thaysang <sanglq@protonmail.com>
2022-10-28 03:18:51 +0000

    mdb03

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'thaysang:mdb',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'Auto create collection database',
   // URL to the Git repository containing the source code for this package.
